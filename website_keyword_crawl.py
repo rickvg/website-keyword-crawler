2a7e6054ee860b65bd31fc3c5426766567fd1a6f
rickvg <rickvgorp@hotmail.com>
2016-05-26 13:01:48 +0200

    Updated to Website Keyword Crawler v1.1
    - Added multithreading
    - Optimized code

--- website_keyword_crawl.py
+++ website_keyword_crawl.py
@@ -1,13 +1,50 @@
 #Python 3.x
-#Website keyword crawler v1.0
+#Website keyword crawler v1.1
 #Created by rickvg @ https://github.com/rickvg
 
 import urllib.request
 import re
+import threading
+from multiprocessing import Queue
+
+
+def findkeywordlvl(strwebsiteinp, strmatch, queueget):
+    if strmatch.startswith("src="):
+        strmatch = strmatch[5:len(strmatch)]
+    elif strmatch.startswith("href="):
+        strmatch = strmatch[6:len(strmatch)]
+
+    if not (strmatch.endswith(".jpg")) or (strmatch.endswith(".png")) or (strmatch.endswith(".bmp")) or (strmatch.endswith(".gif")):
+        if strmatch.startswith("//"):
+            strwebsite2 = "http:" + strmatch
+        elif strmatch.startswith("/"):
+            strwebsite2 = strwebsiteinp + strmatch
+        else:
+            strwebsite2 = strmatch
+        if ("\\" not in strwebsite2):
+            try:
+                print(strwebsite2)
+                strcontent = urllib.request.urlopen(strwebsite2).read()
+                match2 = re.findall(re.escape(strKeyword), str(strcontent))
+                match3 = re.findall("href=[\'\"]http\://[A-z0-9_\-\./]+|href=[\'\"]\/[A-z0-9_\-\./]+|href=[\'\"]www[A-z0-9_\-\./]+",str(strcontent))
+                match3 = match3 + re.findall("src=[\'\"]http\://[A-z0-9_\-\./]+|src=[\'\"]\/[A-z0-9_\-\./]+|src=[\'\"]www[A-z0-9_\-\./]+",str(strcontent))
+                if match2:
+                    strPrint = strwebsite2 + " has " + str(len(match2)) + " matches with keyword: " + strKeyword + "\n"
+                    print(strPrint)
+                    strFile.write(strPrint)
+                else:
+                    print("No matches for:", strwebsite2)
+                queueget.put([strwebsite2, match3])
+                return [strwebsite2, match3]
+            except Exception as ex:
+                errormsg = "Exception {0} occurred. Reason:\n{1!r}"
+                message = errormsg.format(type(ex).__name__, ex.args)
+                print(message)
+                strFile2.write(message)
 
 strWebsite = input("Enter website (Format http://domain.com):\n")
 strKeyword = input("Enter keyword to search for:\n")
-intLevel = int(input("Select levels to scan. Choose 1, 2 or 3 - 3 might contain errors because of websites out of scope.:\n"))
+intLevel = int(input("Select levels to scan. Choose 1, 2 or 3 - 3 might contain errors:\n"))
 filename = strWebsite[7:len(strWebsite)] + " positives.log"
 filename2 = strWebsite[7:len(strWebsite)] + " errors.log"
 strFile = open(filename, 'w')
@@ -26,94 +63,47 @@ else:
 
 if intLevel == 1:
     print("Finished scanning website for keywords")
-elif intLevel >= 2:
+elif intLevel in range(2, 4):
     regex1 = r"src=[\'\"]http\://[A-z0-9_\-\./]+|src=[\'\"]\/[A-z0-9_\-\./]+|src=[\'\"]www[A-z0-9_\-\./]+"
     regex2 = r"href=[\'\"]http\://[A-z0-9_\-\./]+|href=[\'\"]\/[A-z0-9_\-\./]+|href=[\'\"]www[A-z0-9_\-\./]+"
+
+    results = []
+
     match = re.findall(re.compile(regex2), str(strContent))
     matchsrc = re.findall(re.compile(regex1), str(strContent))
     match = match + matchsrc
 
-    for i in range(0,len(match)):
-        if match[i].startswith("src="):
-            match[i] = match[i][5:len(match[i])]
-        elif match[i].startswith("href="):
-            match[i] = match[i][6:len(match[i])]
-        print(match[i])
-        if (match[i].endswith(".jpg")) or (match[i].endswith(".png")) or (match[i].endswith(".bmp")) or (match[i].endswith(".gif")):
-            print("")
-        else:
-            if match[i].startswith("//"):
-                strWebsite2 = "http:" + match[i]
-            elif match[i].startswith("/"):
-                strWebsite2 = strWebsite + match[i]
-            else:
-                strWebsite2 = match[i]
-            print(strWebsite2)
-            if ("\\" not in strWebsite2):
-                try:
-                    strContent = urllib.request.urlopen(strWebsite2).read()
-                    match2 = re.findall(re.escape(strKeyword), str(strContent))
+    q = Queue()
+    threads = []
 
-                    if intLevel == 3:
-                        match3.append(re.findall("href=[\'\"]http\://[A-z0-9_\-\./]+|href=[\'\"]\/[A-z0-9_\-\./]+|href=[\'\"]www[A-z0-9_\-\./]+", str(strContent)))
-                        match3.append(re.findall("src=[\'\"]http\://[A-z0-9_\-\./]+|src=[\'\"]\/[A-z0-9_\-\./]+|src=[\'\"]www[A-z0-9_\-\./]+", str(strContent)))
-                    if match2:
-                        strPrint = strWebsite2 + " has " + str(len(match2)) + " matches with keyword: " + strKeyword + "\n"
-                        print(strPrint)
-                        strFile.write(strPrint)
-                    else:
-                        print("No matches for:", strWebsite2)
-                except Exception as ex:
-                    errormsg = "Exception {0} occured. Reason:\n{1!r}"
-                    message = errormsg.format(type(ex).__name__, ex.args)
-                    print(message)
-                    strFile2.write(message)
-                
+    i = 0
+    while i < len(match):
+        if threading.active_count() < 10:
+            t = threading.Thread(target=findkeywordlvl, args =(strWebsite, match[i],q))
+            t.start()
+            threads.append(t)
+            i += 1
+
+    for p in threads:
+        p.join()
+    while not q.empty():
+        results.append(q.get_nowait())
+
+    print(results)
+
+    threads = []
+    j = 0
     if intLevel == 3:
-        for i in range(0,len(match3)):
-            for j in range(0,len(match3[i])):
-                if match3[i][j].startswith("src="):
-                    match3[i][j] = match3[i][j][5:len(match3[i][j])]
-                elif match3[i][j].startswith("href="):
-                    match3[i][j] = match3[i][j][6:len(match3[i][j])]
-
-                print(match3[i][j])
-
-                if (match3[i][j].endswith(".jpg")) or (match3[i][j].endswith(".png")) or (match3[i][j].endswith(".bmp")) or (match3[i][j].endswith(".ico"))or (match3[i][j].endswith(".gif")):
-                    print("")
-                else:
-                    if match3[i][j].startswith("//"):
-                        strWebsite3 = "http:" + match3[i][j]
-                    elif match3[i][j].startswith("/"):
-                        if strWebsite2.endswith("/"):
-                            strWebsite3 = strWebsite2[0:len(strWebsite2)-1] + match3[i][j]
-                        else:
-                            strWebsite3 = strWebsite2 + match3[i][j]
-                        print(strWebsite3)
-                    
-                    else:
-                        strWebsite3 = match3[i][j]
-                    if ("\\" not in strWebsite3):
-                        try:
-                            strContent = urllib.request.urlopen(strWebsite3).read()
-                            match4 = re.findall(re.escape(strKeyword), str(strContent))
-
-                            if match4:
-                                strPrint = strWebsite3 + " has " + str(len(match4)) + " matches with keyword: " + strKeyword + "\n"
-                                print(strPrint)
-                                strFile.write(strPrint)
-                            else:
-                                print("No matches for:", strWebsite3)
-                        except Exception as ex:
-                            errormsg = "Exception {0} occured. Reason:\n{1!r}"
-                            message = errormsg.format(type(ex).__name__, ex.args)
-                            print(message)
-                            strFile2.write(message)
-    else:
-        print("")
+        for i in range(0,len(results)):
+            while j < len(results[i][1]):
+                if threading.active_count() < 10:
+                    threads.append(threading.Thread(target=findkeywordlvl, args=(results[i][0],results[i][1][j],q)))
+                    threads[j].start()
+                    j += 1
+        for p in threads:
+            p.join()
 else:
     print("Wrong level. Try again.")
-
 
 strFile.close()
 strFile2.close()
